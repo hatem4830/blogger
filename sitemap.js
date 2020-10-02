aaba1703333306744ba104b666ef106e0e53bb1c
hatem4830 <33000397+hatem4830@users.noreply.github.com>
2020-10-02 05:11:32 +0300

    Update sitemap.js

--- sitemap.js
+++ sitemap.js
@@ -270,4 +270,4 @@ function hideToc() {
   tocdiv.innerHTML = '';
   var toclink = document.getElementById("toclink");
   toclink.innerHTML = '<a href="#" onclick="scroll(0,0); showToc(); Effect.toggle('+"'toc-result','blind');"+'">» Show Table of Contents</a> <img src="http://chenkaie.blog.googlepages.com/new_1.gif"/>';
-}
+} 
